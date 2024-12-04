7aa58a4d0e17b588e740e55d92b468639b6daf20
Roberta Bastos Jesus <rbastosdeveloper@gmail.com>
2024-12-03 21:20:42 -0300

    Secound Part

--- src/pages/doados/doados.jsx
+++ src/pages/doados/doados.jsx
@@ -1,10 +1,24 @@
-
-
+import Style from '../doados/doados.module.scss'
+import livro from '../../assets/livro.png'
 
 export default function Doados(){
     return(
-        <section>
-            <h2>EU SOU A DOADOS</h2>
+        <section className={Style.boxDoados}>
+            <h2>Livros Doados</h2>
+            <section className={Style.boxBooks}>
+                <article>
+                    <img src={livro} alt="imagem do livro o protagonista" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
+                <article>
+                    <img src={livro} alt="imagem do livro o protagonista" />
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
+            </section>
         </section>
     )
 }
